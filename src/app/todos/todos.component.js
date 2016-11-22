330994d9e7ba0c4935a09688e9a0b3cbe597754a
josue.riosdiaz <josue.riosdiaz@almundo.com>
2016-11-22 10:49:25 -0300

    todo root component done

--- src/app/todos/todos.component.js
+++ src/app/todos/todos.component.js
@@ -1,3 +1,53 @@
 /**
  * Created by Josue on 22/11/16.
  */
+(function () {
+    'use strict';
+
+    angular
+        .module('todos')
+        .component('todos', {
+            controller: TodosController,
+
+
+            templateUrl: ""
+        });
+
+
+    TodosController.$inject = ['TodoService'];
+
+    /* @ngInject */
+    function TodosController(TodoService) {
+
+        var _self = this;
+        this.todoService = TodoService;
+
+
+        this.$onInit = function () {
+            this.todos = this.todoService.getTodos();
+
+        };
+
+        this.addTodo = function (label) {
+            _self.todos = _self.todos.push({label, id: _self.todos.length + 1});
+        };
+
+        this.completeTodo = function (todo) {
+            _self.todos = _self.todos.map(function (item) {
+                return item.id === todo.id ? Object.assign({}, item, {complete: true}) : item
+            });
+
+        };
+
+        this.removeTodo = function (todo) {
+            _self.todos = _self.todos.filter(function(item){
+               return  todo.id !== item.id;
+            });
+
+        }
+
+
+    }
+
+})();
+
