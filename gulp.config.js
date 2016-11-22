f22d5f07c1d38f73ff4a70d9b7db68a6aed2240b
josue.riosdiaz <josue.riosdiaz@almundo.com>
2016-11-22 09:59:45 -0300

    small folder structure refactor

--- gulp.config.js
+++ gulp.config.js
@@ -1,3 +1,51 @@
 /**
  * Created by Josue on 22/11/16.
  */
+'use strict';
+
+module.exports = function () {
+    const pkg = require('./package.json');
+
+    const server = './server/';
+
+    const scssfolder = `${client }scss/`;
+    const mainscss = `${scssfolder }main.scss`;
+    const build = './build/';
+    const appFolder = `${client}app/`;
+
+    const config = {
+        projectName: pkg.name,
+        version: pkg.version,
+        appFolder,
+        app: `${appFolder }app.module.js`,
+        scssfolder,
+        mainscss,
+        angularRootApp: `${appFolder}core/`,
+        build,
+        images: `${client }images/`,
+        fonts: `${scssfolder }fonts/`,
+        vendorfolder: `${client }vendor/`,
+        templateCache: {
+            file: 'app.templates.js',
+            options: {
+                standalone: true,
+                module: 'app.templates',
+                templateHeader: 'export const AppTemplates = ' +
+                ' angular.module("<%= module %>"<%= standalone %>)' +
+                '.run(["$templateCache", function($templateCache) {',
+                templateFooter: '}]).name;'
+            }
+        },
+        jsOrder: ['**/app.module.js', '**/*.module.js', '**/*.js'],
+        browserReloadDelay: 1000,
+        files: {
+            sass: `${client}**/*.scss`,
+            js: [`${client}**/*.js`, `!${client}**/*.templates.js`],
+            html: `${client}**/*.html`,
+            jade: `${client}**/*.jade`
+
+        }
+    };
+
+    return config;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,58 @@
 /**
  * Created by Josue on 22/11/16.
  */
+const config = require('./gulp.config.js')();
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const plugs = gulpLoadPlugins();
+
+const argv = require('yargs').argv;
+const env = process.env.NODE_ENV || 'local';
+let optimize = false;
+
+
+
+if (env === 'staging' || env === 'production') {
+    log('static files will be optimized!!');
+    optimize = true;
+}
+
+/**
+ * Compiling jade into html for components.
+ */
+gulp.task('compile-jade', () => {
+    log('compiling jade into html');
+
+
+    return gulp
+        .src(`${config.appFolder }**/*.jade`)
+        .pipe(plugins.jade())
+        .pipe(plugins.htmlmin())
+        .pipe(gulp.dest(config.appFolder));
+});
+
+
+gulp.task('template-cache', ['compile-jade'], () => {
+
+
+    return gulp.src(`${config.appFolder }**/*.html`)
+        .pipe(plugins.angularTemplatecache(config.templateCache.file, config.templateCache.options))
+        .pipe(gulp.dest(config.appFolder));
+});
+
+
+/**
+ * Compiling scss into css.
+ */
+gulp.task('styles-app', () => {
+    log('compiling app scss into css');
+    return gulp
+        .src(config.mainscss)
+        .pipe(plugins.sass())
+        .pipe(plugins.rename(`${config.projectName}.css`))
+        .pipe(plugins.if(optimize, plugins.combineMq()))
+        .pipe(plugins.if(optimize, plugins.csso()))
+        .pipe(plugins.if(optimize, plugins.rev()))
+        .pipe(gulp.dest(config.build));
+});
+
